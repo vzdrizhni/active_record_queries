c933e77522014d8f3c4357591791e2fdaa63c3e6
vzdrizhni <vzdrizhni@gmail.com>
2024-10-18 16:20:36 +0400

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
-    render json: user, include: ['orders', 'orders.products']
+    render json: user
   end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name
 
   has_many :orders
+  has_many :products
 end
